e9da2fc5ff393511ff575e54c74fef7860c980f5
Deriks Karlay Dias Costa <karlaycosta@gmail.com>
2022-08-29 18:04:17 -0300

    29-08-2022_3

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -7,33 +7,52 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textCep = TextEditingController();
+    final formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Consulta VIACEP'),
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: 400,
-              child: TextFormField(
-                controller: textCep,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  label: Text('CEP'),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 400,
+                child: TextFormField(
+                  controller: textCep,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    label: Text('CEP'),
+                  ),
+                  validator: (valor) {
+                    if (valor == null || valor.isEmpty) {
+                      return 'Você deve informar um CEP! SEU ANIMAL';
+                    }
+                    if (valor.length != 8) {
+                      return 'Você deve informar um CEP com 8 dígitos!';
+                    }
+                    if (!validarCep(valor)) {
+                      return 'Cep inválido';
+                    }
+                    // Passou na validação
+                    return null;
+                  },
                 ),
               ),
-            ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-              child: const Text('Consultar'),
-              onPressed: () {
-                consultar(textCep.text);
-              },
-            ),
-          ],
+              const SizedBox(height: 40),
+              ElevatedButton(
+                child: const Text('Consultar'),
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    consultar(textCep.text);
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/services/cep.dart
+++ lib/services/cep.dart
@@ -1,10 +1,51 @@
 import 'package:http/http.dart';
 
 void consultar(String cep) async {
-  final res = await get(Uri.https('viacep.com.br', '/ws/$cep/json/'));
-  if (res.statusCode == 200) {
-    print(res.body);
-  } else {
-    print('Ocorreu um erro!');
+  if (cep.isEmpty) {
+    print('Envia o CEP seu animal!');
+    return;
   }
+  if (validarCep(cep)) {
+    try {
+      final res = await get(Uri.https('viacep.com.br', '/ws/$cep/json/'));
+      print(res.body);
+    } catch (e) {
+      print('Ocorreu um erro!');
+    }
+  } else {
+    print('CEP inválido');
+  }
+}
+
+bool validarCep(String cep) {
+  if (cep.length == 8) {
+    for (var i = 0; i < cep.length; i++) {
+      switch (cep[i]) {
+        case '0':
+          continue;
+        case '1':
+          continue;
+        case '2':
+          continue;
+        case '3':
+          continue;
+        case '4':
+          continue;
+        case '5':
+          continue;
+        case '6':
+          continue;
+        case '7':
+          continue;
+        case '8':
+          continue;
+        case '9':
+          continue;
+        default:
+          return false;
+      }
+    }
+    return true;
+  }
+  return false;
 }
